404fa32f4cee5119959152c38e6f904570b71d97
Sonu Jha <99082963+Issonujha@users.noreply.github.com>
2025-01-26 18:18:11 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,7 +3,7 @@ withCredentials([usernamePassword("credentialsId":"connectToDockerStudy",
                     passwordVariable:"dockerHubPass", 
                     usernameVariable:"dockerHubUser")]) {
                     sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-                    sh "docker image tag notes-app:latest ${dockerHubUser}/{$project}:${imageTag}"
+                    sh "docker image tag notes-app:latest ${dockerHubUser}/${project}:${imageTag}"
                     sh "docker push ${dockerHubUser}/{$project}:${imageTag}"
                 }
 }
